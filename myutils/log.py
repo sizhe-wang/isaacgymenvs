bfe4d4e8ce43f6829bd2cb01c20aaadafc52443c
wsz <sizhe_wang@163.com>
2021-11-09 21:31:48 +0800

    path:isaacgym/python/examples

---
+++ myutils/log.py
@@ -0,0 +1,13 @@
+import os
+from isaacgym import gymapi
+from isaacgym import gymutil
+from isaacgym import gymtorch
+from isaacgym.torch_utils import *
+
+
+import math
+import numpy as np
+import torch
+
+
+# TODO: write diverse functions to print something to log file
